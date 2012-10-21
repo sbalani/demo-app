2a2eff484b3619c42db25ed5e6d426eb17ccfc0a
sbalani <sharan.balani@ripple-wave.com>
2012-10-21 19:44:50 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '62b9a9157dad67f9441d15f373744aa3d0eebefc6c898dccf9b381393f167dcd73f9befd4affae5d8dab53e079cd2a8bcff5e83aaca3038385cf223a8edc52ee'
